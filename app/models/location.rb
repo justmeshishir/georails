4176bb95462833376c83011b766d9579297b4ced
Shishir Thapa <shishirthapa1@gmail.com>
2016-12-31 17:38:35 +0000

    map styling

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,6 @@
 class Location < ActiveRecord::Base
     geocoded_by :address
     after_validation :geocode, if: :address_changed?
-    validate :latitude, :longitude
+    validates :address, presence: true
+    
 end
